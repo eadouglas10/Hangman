f45f6d8dd66e0c076aa579df50c4331a26b2d494
Erik Douglas <eadouglas10@gmail.com>
2016-06-22 19:18:19 -0400

    Modified puts and print statemtents

--- Hangman.rb
+++ Hangman.rb
@@ -10,12 +10,13 @@ found_letters = []
 other_letters_tried = []
 
 
-puts "Let's play hangman! Type in a letter to make a guess"
+puts "Let's play hangman! The word has #{len} letters. Type in a letter to make a guess."
 
 while(wrong_guesses_remaining > 0 && !answer_found)
   try = gets.chomp
   found_a_letter = false
   characters_found = 0
+  answer_found = true
 
     if(try.to_i.to_s != try && try.to_f.to_s != try && try.length == 1)#single letter entered
       for x in 0...len
@@ -25,11 +26,23 @@ while(wrong_guesses_remaining > 0 && !answer_found)
           characters_found += 1
         end
       end
-      puts "The letter " + try + " occurs " + characters_found.to_s + " times."
+
+      puts ""
+      print "The letter " + try + " occurs " + characters_found.to_s + " time"
+      if characters_found == 1
+        puts "s."
+      else
+        puts "."
+      end
+
       if found_a_letter == false
+        if !other_letters_tried.include?(try)
+          other_letters_tried << try
+        end
         wrong_guesses_remaining -= 1
         puts "You have " + wrong_guesses_remaining.to_s + " wrong guesses remaining."
       end
+
     else
       puts "Not a valid guess!"
       wrong_guesses_remaining -= 1
@@ -40,7 +53,19 @@ while(wrong_guesses_remaining > 0 && !answer_found)
         print word_chars[x]
       else
         print "_"
+        answer_found = false
       end
     end
 
+    puts ""
+    print "You have ruled out these letters: "
+    for x in 0...other_letters_tried.length
+      print other_letters_tried[x] + " "
+    end
+    puts ""
+    puts "Try another one!"
+end
+
+if answer_found
+  puts "That's it!"
 end
